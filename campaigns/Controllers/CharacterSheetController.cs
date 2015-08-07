f5b29eea6aa07a1decef6493789eb11959c83945
seshbot <seshbot-github@cechner.com>
2015-08-07 11:07:10 +0800

    added angular and charactersheet controller placeholder

---
+++ campaigns/Controllers/CharacterSheetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace campaigns.Controllers
+{
+    public class CharacterSheetController : Controller
+    {
+        // GET: CharacterSheet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
